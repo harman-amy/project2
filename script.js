3866993275d65181d7425fc2a5657f398978a925
harman <harmanave@gmail.com>
2021-02-21 21:17:35 +0530

    updated js and styles & organized the code

--- script.js
+++ script.js
@@ -1,127 +1,106 @@
-console.log("checking");
 
-const weatherApp = {};
+// creating an app object
+    const weatherApp = { };
 
-weatherApp.apiUrl = "https://api.openweathermap.org/data/2.5/weather";
-weatherApp.apiKey = "75bca0532ad0bc22fa4ca7f257f8dbfe";
+    //save relevant API information
+    weatherApp.apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+    weatherApp.apiKey = "75bca0532ad0bc22fa4ca7f257f8dbfe";
 
-weatherApp.getCities = function () {
-    const url = new URL(weatherApp.apiUrl);
-    url.search = new URLSearchParams({
-        // pass in our API key as a parameter
-        q: userChoice,
-        appid: weatherApp.apiKey
-    })
-    console.log(url);
+    // event handler on form
 
-    fetch(url)
-        .then(function (response) {
-            // console.log(response);
-            // parse this response into JSON
-            return response.json();
-            // return JSON response so that it can be used in the next function
-        })
-        // parse the JSON promise resposne and log out readable data (aka data in JSON format)
-        .then(function (jsonResponse) {
-            console.log(jsonResponse);
-            
-        const formElement = document.querySelector('form');
-        formElement.addEventListener('submit', function (event) {
-        event.preventDefault();
+    const formElement = document.querySelector('form');
+    formElement.addEventListener('submit', function (event) {
+    event.preventDefault();
+        // prevent page from refreshing
 
-            if(userChoice === ""){
-                alert("Please enter a city");
-            }
-            
-            // object to hold all the temperatures
+        //2. to get the user input from the form
+        const inputElement = document.querySelector('input');
+        // console.log(inputElement.value)
+        // capturing the value from the input on submit.
 
-            const tempObj = {
-                temperature: jsonResponse.main.temp,
-                feelsTemperature: jsonResponse.main.feels_like,
-                minTemperature: jsonResponse.main.temp_min,
-                maxTemperature: jsonResponse.main.temp_max,
-            }
+        //3. storing input into a variable
+        const cityName = inputElement.value;
+        console.log(cityName)
 
-            console.log(tempObj);
-
-              // Temp conversion function
-
-            function converter(value) {
-                for (let properties in tempObj) {
-                    properties = Math.round(tempObj[properties] - 273.15);
-                    console.log(properties);
-                }
-                // jsonResponse.main.temp_max = Math.round(value - 273.15);
-                // console.log(value - 273.15);
-            }
-
-            converter();
-
-            // work with the data from the API
-
-            const displayWeather = document.querySelector('.weather-data');
-            displayWeather.textContent = `${userChoice}`;
-            // console.log()
-
-            const displayTemp = document.querySelector('.weather-data');
-            const paraTemp = document.createElement('p');
-            paraTemp.textContent = `Temperature:${jsonResponse.main.temp}`;
-            document.querySelector('.weather-data').appendChild(paraTemp);
-
-            const feelsTemp = document.createElement('p');
-            feelsTemp.textContent = `Feels like:${jsonResponse.main.feels_like}`;
-            document.querySelector('.weather-data').appendChild(feelsTemp);
-
-            const minTemp = document.createElement('p');
-            minTemp.textContent = `Minimum Temperature:${jsonResponse.main.temp_min}`;
-            document.querySelector('.weather-data').appendChild(minTemp);
-
-            const maxTemp = document.createElement('p');
-            maxTemp.textContent = `Maximum Temperature:${jsonResponse.main.temp_max}`;
-            document.querySelector('.weather-data').appendChild(maxTemp);
-
-            const paraHumidity = document.createElement('p');
-            paraHumidity.textContent = `Humidity:${jsonResponse.main.humidity}`;
-            document.querySelector('.weather-data').appendChild(paraHumidity);
-            
-            const weatherIcon = document.createElement('i');
-            weatherIcon.textContent = jsonResponse.weather[0].icon;
-            document.querySelector('.weather-data').appendChild(weatherIcon);
-
-            const weatherDescription = document.createElement('p');
-            weatherDescription.textContent = jsonResponse.weather[0].description;
-            document.querySelector('.weather-data').appendChild(weatherDescription);
-
-            
-
-            // function kelvinToCelcius(value) {
-
-            //     let paraTempC = parseFloat(paraTemp);
-            //     paraTempC = paraTempC - 273.15;
-            //     console.log(paraTempC);
-            //     }
-            //   kelvinToCelcius(300);
+        //create a method (AKA function on the app object) which requests information from the API
+        //logs it to the console
+        weatherApp.getApiInfo = function () {
+            // use the URL constructor to specify the parameters we wish to inculde in our API endpoint (aka in the request we are making in the API.)
+            const url = new URL(weatherApp.apiUrl);
+            url.search = new URLSearchParams({
+            appid: weatherApp.apiKey,
+                q: inputElement.value,
             })
-        })
-        
-}
+
+            // console.log(url);
+
+            //using the fetch API to make a request to the Unsplash API photos endpoint
+            // pass in new URL featuring params provided by the URL search params constructor.
+            fetch(url)
+                .then(function (response) {
+                    // console.log("hello");
+                    // get data from the url. To do that, we will add a parameter "RESPONSE" which will define the data from url.
+
+                    // console.log(response);
+                    // so the data we get back from url is response OBJECT but is not exactly contains the information we wanrs, so we will parse or extract data into JSON form.
+
+                    // parse this response into JSON
+                    return response.json();
+                    // the above method lives within fetch request that will turn the data into JSON file.
+
+                    // return JSON response so that it can be used in the next function
+                })
+
+                // after we get back data in JSON file, then do the following.
+                .then(function (jsonResponse) {
+    // jsonResponse will represent the data from JSON file
+    console.log(jsonResponse);
+
+
+                    // display the data in this method or call the displayApiinfo() method within this function after its completed.
+                    // because we need info from api(jsonResponse) from this method(getApiInfo) for method(displayApiInfo).
+
+                    weatherApp.displayApiInfo(jsonResponse);
+                })
+        }
+
+        // Define a method to display jsonResponse from Api.
+        weatherApp.displayApiInfo = function (jsonData) {
+            // jsonData is represnting data from jsonResponse as we will end up calling this method within getApiInfo method because otehrwise we wont have access to that information as its locally scoped in getApiInfo and we will end up calling this method within getApiInfo.
+
+            // query the document and find the (div class = "display-weather") where we will contain the dynamic content.
+            const divElement = document.querySelector('.display-weather');
+            // console.log(divElement);
+
+            // create a 'h1' tag to contain the city name.
+            const h1Element = document.createElement('h1');
+            h1Element.textContent = `${jsonData.name}, ${jsonData.sys.country}`;
+            // console.log(h1Element);
+            divElement.appendChild(h1Element);
+
+            // create h2 tag to hold temperature information
+            const h2Element = document.createElement('h2');
+            h2Element.textContent = jsonData.main.temp;
+            divElement.appendChild(h2Element);
+
+
+            // create few 'p' tags to contain information like temperature.
+
+            const paragraph = document.createElement('p');
+            // Once we create elements, we need to append the elements to the div class = "display-weather"
+        }
+
+        weatherApp.getApiInfo()
+
+        //clear the input after the event is saved & acted upon.
+        inputElement.value = "";
+    })
 
 
 
-const inputElement = document.querySelector('input');
-const userChoice = inputElement.value;
-// to store the city chosen by user.
 
-// const buttonElement = document.querySelector('button');
+    // weatherApp.init = function () {
 
+    // }
 
-// on submit,  weather info - temp/humidity icon, description 
-
-
-//create an initialization method
-weatherApp.init = function () {
-    //calling the method which makes the request to the API
-    weatherApp.getCities();
-}
-//call the init method to kickstart our app
-weatherApp.init();
+    // weatherApp.init();
