ff8bc856a43a0e8b858d62027adf066fd1c12222
harman <harmanave@gmail.com>
2021-02-23 05:18:52 +0530

    media queries and fixed background image

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-// creating an app object
+/// creating an app object
 const weatherApp = {};
 
 //save relevant API information
 weatherApp.apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 weatherApp.apiKey = "75bca0532ad0bc22fa4ca7f257f8dbfe";
 
+//create a method (AKA function on the app object) which requests information from the API
+//logs it to the console
 weatherApp.getApiInfo = function (userChoice) {
   // use the URL constructor to specify the parameters we wish to inculde in our API endpoint (aka in the request we are making in the API.)
   const url = new URL(weatherApp.apiUrl);
   url.search = new URLSearchParams({
     appid: weatherApp.apiKey,
-    q: userChoice
+    q: userChoice,
   })
+
   // console.log(url);
+
+  //using the fetch API to make a request to the Unsplash API photos endpoint
   // pass in new URL featuring params provided by the URL search params constructor.
   fetch(url)
     .then(function (response) {
@@ -37,60 +42,92 @@ weatherApp.getApiInfo = function (userChoice) {
       // display the data in this method or call the displayApiinfo() method within this function after its completed.
       // because we need info from api(jsonResponse) from this method(getApiInfo) for method(displayApiInfo).
 
+      updateBackground(jsonResponse);
+
       weatherApp.displayApiInfo(jsonResponse);
       // weatherApp.displayApiInfo(jsonResponse);
 
     })
 }
 
+// function for updating background images based on weather conditions 
+function updateBackground(jsonDataIf) {
+
+  const headerElement = document.querySelector('header');
+  // console.log(headerElement);
+
+  if (jsonDataIf.weather[0].main == "Clear") {
+    headerElement.setAttribute('class', 'header-clear');
+  } else if (jsonDataIf.weather[0].main == "Clouds") {
+    headerElement.setAttribute('class', 'header-cloudy');
+  } else if (jsonDataIf.weather[0].main == "Rain") {
+    console.log("Display Rainy image");
+  } else if (jsonDataIf.weather[0].main == "Snow") {
+    console.log("Display Snowy image");
+  } else if (jsonDataIf.weather[0].main == "Fog") {
+    console.log("Display Foggy image");
+  } else {
+    console.log("Display clear image");
+  }
+}
+
+// function for error message
+
+
 // Define a method to display jsonResponse from Api.
 weatherApp.displayApiInfo = function (jsonData) {
   // jsonData is represnting data from jsonResponse as we will end up calling this method within getApiInfo method because otehrwise we wont have access to that information as its locally scoped in getApiInfo and we will end up calling this method within getApiInfo.  
 
   // query the document and find the (div class = "display-weather") where we will contain the dynamic content.
-  const divElement = document.querySelector('.display-weather');
+  const weatherDiv = document.querySelector('.weather');
   // console.log(divElement);
-  divElement.innerHTML = "";
+
+  weatherDiv.innerHTML = "";
 
   // create a 'h1' tag to contain the city name.
   const h1Element = document.createElement('h1');
   h1Element.textContent = `${jsonData.name}, ${jsonData.sys.country}`;
   // console.log(h1Element);
-  divElement.appendChild(h1Element);
+  weatherDiv.appendChild(h1Element);
+
+  const weatherDescription = document.createElement('h3');
+  weatherDescription.textContent = jsonData.weather[0].description;
+  weatherDescription.setAttribute('class', 'capitalize');
+  weatherDiv.appendChild(weatherDescription);
 
   // create h2 tag to hold temperature information
   const currentTemp = document.createElement('h2');
-  currentTemp.textContent = `${Math.round(jsonData.main.temp - 273.15)} °C`;
-  divElement.appendChild(currentTemp);
+  currentTemp.textContent = `${Math.round(jsonData.main.temp - 273.15)}°C`;
+  currentTemp.setAttribute('class', 'temperature');
+  weatherDiv.appendChild(currentTemp);
 
-  // create <i> to hold weather icon
-  const weatherIcon = document.createElement('p');
-  weatherIcon.textContent = jsonData.weather[0].icon;
-
-  const iconUrl = document.createElement('url');
+  // create <div> to hold weather icon
+  const iconUrl = document.createElement('div');
   iconUrl.innerHTML = `<img src= http://openweathermap.org/img/w/${jsonData.weather[0].icon}.png>`;
-  divElement.appendChild(iconUrl);
+  weatherDiv.appendChild(iconUrl);
 
+  // query the document and find the (div class = "display-weather-conditions") where we will contain the weather-condtions content.
+  const conditionsDiv = document.querySelector('.weather-conditions')
+  conditionsDiv.innerHTML = "";
   // create h3 tags for information like temp feels and humidity.
 
   const feelsTemp = document.createElement('h3');
   feelsTemp.textContent = `Feels like: ${Math.round(jsonData.main.feels_like - 273.15)} °C`;
-  divElement.appendChild(feelsTemp);
+  conditionsDiv.appendChild(feelsTemp);
 
   const humidity = document.createElement('h3');
   humidity.textContent = `Humidity: ${jsonData.main.humidity}%`;
-  divElement.appendChild(humidity);
+  conditionsDiv.appendChild(humidity);
 
-  const weatherDescription = document.createElement('h4');
-  weatherDescription.textContent = jsonData.weather[0].description;
-  divElement.appendChild(weatherDescription);
+  const wind = document.createElement('h3');
+  wind.textContent = `Wind: ${jsonData.wind.speed}km/h`;
+  conditionsDiv.appendChild(wind);
 
-  const paragraph = document.createElement('p');
-  // Once we create elements, we need to append the elements to the div class = "display-weather" 
-
+  // Once we create elements, we need to append the elements to the div class = "display-weather"
 }
 
-// event handler on form
+// event handler on form to get userChoice
+
 const formElement = document.querySelector('form');
 formElement.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -103,19 +140,23 @@ formElement.addEventListener('submit', function (event) {
 
   //3. storing input into a variable
   const cityName = inputElement.value;
+  // cityName.setAttribute('class', 'cityname');
   // console.log(cityName)
 
-  //create a method (AKA function on the app object) which requests information from the API
-  //logs it to the console
+  // errorMessage(inputElement.value);
+
   weatherApp.getApiInfo(inputElement.value)
+
+  //clear the input after the event is saved & acted upon. 
   inputElement.value = "";
 })
 
 weatherApp.init = function () {
-  weatherApp.getApiInfo('toronto')
+  weatherApp.getApiInfo("toronto");
+  // we are running this function because we need Toronto data on default load screen.
 }
-weatherApp.init();
 
+weatherApp.init();
 
 
       
